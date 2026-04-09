a6d9d5b4ca63d89bc370667e0f2b9be79143c588
Jericho Jake Guevara <jerichojakeguevara14@gmail.com>
2026-04-09 09:43:00 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
